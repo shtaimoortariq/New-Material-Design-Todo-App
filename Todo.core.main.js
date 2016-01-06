e760ce3f247ddb1ea06303ffd1e0c9f3ec5a9403
taimoor123 <taimoor_t@yahoo.com>
2016-01-06 15:21:06 +0500

    Initial work with testing

--- Todo.core.main.js
+++ Todo.core.main.js
@@ -1,3 +1,14 @@
 /**
  * Created by Taimoor tariq on 1/6/2016.
  */
+
+
+var app = angular.module("todoCoreModule", ['ngMaterial']);
+
+app.controller("todoCoreController", function () {
+
+
+
+
+
+});
